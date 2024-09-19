853c4a5b3a1a60b618f74a156f8ee9372ed88b77
Salah <salah@Salahs-Laptop.local>
2024-09-19 08:22:25 +0300

    intial

--- src/features/cart/CartOverview.jsx
+++ src/features/cart/CartOverview.jsx
@@ -6,17 +6,18 @@ function CartOverview() {
   const { cart } = useSelector((store) => store.cartSlice);
   const totalPrice = cart.reduce((a, b) => a + b.totalPrice, 0);
   const quantity = cart.reduce((a, b) => a + b.quantity, 0);
-  return (
-    <div className="bg-slate-900 px-4 py-4 flex items-center justify-between">
-      <p className="text-slate-100 space-x-4 text-sm">
-        <span>{quantity} pizzas</span>
-        <span>{funCurrency(totalPrice)}</span>
-      </p>
-      <Link className="text-slate-100 text-base" to="/cart">
-        Open cart →
-      </Link>
-    </div>
-  );
+  if (cart.length > 0)
+    return (
+      <div className="bg-slate-900 px-4 py-4 flex items-center justify-between">
+        <p className="text-slate-100 space-x-4 text-sm">
+          <span>{quantity} pizzas</span>
+          <span>{funCurrency(totalPrice)}</span>
+        </p>
+        <Link className="text-slate-100 text-base" to="/cart">
+          Open cart →
+        </Link>
+      </div>
+    );
 }
 
 export default CartOverview;
